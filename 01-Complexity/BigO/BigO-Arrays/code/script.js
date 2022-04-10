4e245b2aedd5c3d54edbf675366199c9e655eae1
akash19here <spacex3nasa@gmail.com>
2022-04-10 12:36:11 +0530

    Changed comments in the code

--- 01-Complexity/BigO/BigO-Arrays/code/script.js
+++ 01-Complexity/BigO/BigO-Arrays/code/script.js
@@ -2,5 +2,5 @@ let names = ["Michael", "Melissa", "Andrea"];
 
 let values = [true, {}, [], 2, "awesome"];
 
-//While accessing data in an array if you have the right index of the element you can jump directly to it , array would't go through every single element of array.
+//While accessing data in an array if you have the correct index of the element you can jump directly to it , array would't go through every single element in it.
 //Thats why it has constant time complexity while accessing the data 
